87d91995a1c0b714606da6881e154ffd753158b9
binal082002 <binal082002@gmail.com>
2025-04-15 18:54:22 +0530

    mongoose connection fix

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,7 @@ let gfsBucket = null;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     const db = mongoose.connection.db;
     const bucket = new mongoose.mongo.GridFSBucket(db, {
